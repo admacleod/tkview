7dadfbae3a05bce3d76b829ad1670ac98fe3a475
Alisdair MacLeod <git@alisdairmacleod.co.uk>
2025-08-27 09:01:30 +0100

    Basic UI layout
    This is the main structure into which we can start to load our data.

--- main.go
+++ main.go
@@ -6,8 +6,32 @@ import (
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	// Init main frames
+	organisationBox := tview.NewBox().
+		SetBorder(true).
+		SetTitleAlign(tview.AlignLeft).
+		SetTitle("Organisations")
+	agentBox := tview.NewBox().
+		SetBorder(true).
+		SetTitleAlign(tview.AlignLeft).
+		SetTitle("Agents")
+	executionBox := tview.NewBox().
+		SetBorder(true).
+		SetTitleAlign(tview.AlignLeft).
+		SetTitle("Executions")
+
+	// Lay out the frames
+	topFlex := tview.NewFlex()
+	topFlex.AddItem(organisationBox, 0, 1, false)
+	topFlex.AddItem(agentBox, 0, 1, false)
+
+	flex := tview.NewFlex()
+	flex.SetDirection(tview.FlexRow)
+	flex.AddItem(topFlex, 8, 1, false)
+	flex.AddItem(executionBox, 0, 1, true)
+
+	// Start 'er up!
+	if err := tview.NewApplication().SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
 }
